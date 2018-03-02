c67c27a929b60f250db2626a8a1d3387ca183484
FredPizarro <freddy_ps_3@hotmail.com>
2018-03-02 15:16:59 -0500

    update project in github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+/* Rutes */
+import { app_routing } from './app.routes';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { LoginComponent } from './login/login.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { PaginasComponent } from './pages/paginas.component';
+import { GraphicsComponent } from './pages/graphics/graphics.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NopagefoundComponent,
+    LoginComponent,
+    ProgressComponent,
+    PaginasComponent,
+    GraphicsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    app_routing
   ],
   providers: [],
   bootstrap: [AppComponent]
